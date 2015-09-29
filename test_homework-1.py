4bb2f75a756d05df2bec125e3b80d2a199ea73c7
daria <dmorgun@cogniance.com>
2015-09-29 18:53:03 +0300

    Added more negative tests

--- test_homework-1.py
+++ test_homework-1.py
@@ -9,6 +9,7 @@ payload={	"name": "Chupacabra", 	"position": "Senior QA" }
 payload_no_position = { "name": "GuyThatCanDoAnything" }
 payload_blank_name = { "name": "", "position": "CEO" }
 payload_special_chars = { "name": "~!@#$%^&*()" }
+payload_null = { "name": None }
 headers={'content-type': 'application/json'}
 
 def test_get_candidates():
@@ -58,17 +59,30 @@ def test_adding_candidate_with_blank_name():
     r = requests.post(url+'candidates', data=json.dumps(payload_blank_name), headers=headers)
     assert r.status_code == 400, "Return status code 400 if user tries to add candidate with blank 'name'"
 
+# This test fails and I think it's a bug
 def test_adding_candidate_with_special_chars_name():
     r = requests.post(url+'candidates', data=json.dumps(payload_special_chars), headers=headers)
     assert r.status_code == 400, "Return status code 400 if user tries to add candidate with name that contains special characters"
 
-# def test_adding_name_null():
-#
-#
-# def test_adding_name_256():
-#
-#
-# def test_delete_non_existing_candidate():
+# This test fails and I think it's a bug
+def test_adding_name_null():
+    r = requests.post(url+'candidates', data=json.dumps(payload_null), headers=headers)
+    assert r.status_code == 400, "Return status code 400 if user tries to add candidate with name = null (None in Python)"
+
+def test_adding_name_256():
+    i = 0
+    str256 = 'A'
+    while i<=254:
+        str256 += 'A'
+        i+=1
+    payload256 = {"name": str256, "position": str256}
+    r = requests.post(url+'candidates', data=json.dumps(payload256), headers=headers)
+    assert r.status_code == 201, "Return status code 201 if user tries to add candidate whose name and position is 256 chars long"
+
+# I used candidate with id=1 because he's not present in candidates list and each new candidate's id is incremented, so there will never be second user with id=1
+def test_delete_non_existing_candidate():
+    r = requests.delete(url+'candidates/1')
+    assert r.status_code == 404, "Return status code 404 if user tries to delete candidate that doesn't exist"
 
 if __name__ == '__main__':
     pytest.main([__file__, '-v'])
